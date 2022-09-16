3b07d2ea7700feac11b76cc59dbb2d5bdcc2f44d
Hark25 <harklit25@gmail.com>
2022-09-16 12:23:58 -0700

    file fix to merge

--- src/JSort.java
+++ src/JSort.java
@@ -1,14 +1,3 @@
 public class JSort {
-    public static void main(String[] args) {
- 
-    }
-
-    public static void BubbleSort(int[] arr){
-
-    }
-
-    public static void SelectionSort(int[] arr){
-        
-    }
-
+    
 }
