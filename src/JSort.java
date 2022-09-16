ec4331ee669a74f41c685e85f89af75935f92b89
Hark25 <harklit25@gmail.com>
2022-09-16 12:39:33 -0700

    Created function

--- src/JSort.java
+++ src/JSort.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public static void SelectionSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+        int min = arr[i];
+        int minId = i;
+
+        for (int j = i+1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                minId = j;
+            }
+        }
+        // swapping
+        int temp = arr[i];
+        arr[i] = min;
+        arr[minId] = temp;
     }
 }
