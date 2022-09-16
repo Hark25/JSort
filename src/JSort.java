4d689127008a4871f3c2fe193b0d9b9e5b0238ce
Hark25 <harklit25@gmail.com>
2022-09-16 12:43:00 -0700

    final code

--- src/JSort.java
+++ src/JSort.java
@@ -1,14 +1,21 @@
 
 public class jSort {
     public static void main(String[] args){
-    int list[] = new int[5];
-    list[0] = 5;        
-    list[1] = 8;
-    list[2] = 2;
-    list[3] = 4;
-    list[4] = 7;
-    
+        int list[] = new int[5];
+        int List[] = new int[5];
+        list[0] = 5;
+        list[1] = 8;
+        list[2] = 2;
+        list[3] = 4;
+        list[4] = 7;
+  
+        List[0] = 5;
+        List[1] = 8;
+        List[2] = 2;
+        List[3] = 4;
+        List[4] = 7;
     }
+
     public static void BubbleSort(int[] arr) {
         
         boolean sorted = false;
@@ -23,6 +30,24 @@ public class jSort {
                     sorted = false;
                 }
             }
+        }
+    }
+
+    public static void SelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int minId = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minId = j;
+                }
+            }
+            // swapping
+            int temp = arr[i];
+            arr[i] = min;
+            arr[minId] = temp;
         }
     }
 }
